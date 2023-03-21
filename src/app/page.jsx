ff61165020ecba9901c56c905c8c0abead90b263
Ersan Karimi <ersankarimii311@gmail.com>
2023-03-21 23:56:16 +0800

    refactor: implement the `headings` component from `ariakit` for `headings` element

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { Heading, HeadingLevel } from "ariakit/heading";
 import Image from "next/image";
 import Link from "next/link";
 import { MdArrowForward } from "react-icons/md";
@@ -10,19 +13,23 @@ const Home = () => (
     <section className="container m-auto grid grid-rows-[max-content_1fr] gap-12 px-8 md:grid-rows-none md:px-12 lg:grid-cols-[1.5fr_1fr]">
       <div className="flex h-max flex-col gap-8">
         <div className="flex flex-col gap-4">
-          <h2 className="font-semibold uppercase text-inquares-pizazz-600">
-            Himpunan Mahasiswa Informatika
-          </h2>
-          <h1 className="text-4xl font-bold tracking-wider text-inquares-hippie-green-700 md:text-5xl 2xl:text-6xl">
-            Departemen Pengembangan Minat dan Bakat
-          </h1>
-          <p className="text-lg 2xl:w-5/6">
-            Pengembangan minat dan bakat adalah proses meningkatkan kemampuan
-            individu dalam bidang yang sesuai dengan minat atau kecenderungan
-            alaminya. Hal ini bertujuan untuk mengoptimalkan potensi dan
-            kreativitas individu, sehingga mampu mencapai prestasi yang lebih
-            baik dalam bidang yang diminatinya.
-          </p>
+          <HeadingLevel>
+            <HeadingLevel>
+              <Heading className="font-semibold uppercase text-inquares-pizazz-600">
+                Himpunan Mahasiswa Informatika
+              </Heading>
+            </HeadingLevel>
+            <Heading className="text-4xl font-bold tracking-wider text-inquares-hippie-green-700 md:text-5xl 2xl:text-6xl">
+              Departemen Pengembangan Minat dan Bakat
+            </Heading>
+            <p className="text-lg 2xl:w-5/6">
+              Pengembangan minat dan bakat adalah proses meningkatkan kemampuan
+              individu dalam bidang yang sesuai dengan minat atau kecenderungan
+              alaminya. Hal ini bertujuan untuk mengoptimalkan potensi dan
+              kreativitas individu, sehingga mampu mencapai prestasi yang lebih
+              baik dalam bidang yang diminatinya.
+            </p>
+          </HeadingLevel>
         </div>
         <Link
           href="/"
