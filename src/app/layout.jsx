4555062427a912a486961c07c4f6bd1fe71bba0d
Ersan Karimi <ersankarimii311@gmail.com>
2023-03-21 16:39:24 +0800

    refactor: update the root layout contents
    Changing the wrong attribute `variants` to `weight` prop and the shortcut icon
    
    Implement the `Header` component.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,22 +1,32 @@
 import { Open_Sans } from "next/font/google";
 
 import "./globals.css";
+import { Header } from "./header";
 
-const openSans = Open_Sans({
+export const openSans = Open_Sans({
   subsets: ["latin"],
-  variants: ["400", "500", "600", "700", "800"],
+  weight: ["400", "500", "600", "700", "800"],
   variable: "--font-open-sans",
 });
 
 export const metadata = {
   title: "PMB HMIF 2023",
   description: "Website Departemen Pengembangan Minat dan Bakat HMIF 2023",
+  icons: {
+    icon: "/logo-pmb.png",
+    shortcut: "/logo-pmb.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={openSans.className}>{children}</body>
+      <body
+        className={`relative min-h-screen w-screen max-w-[100vw] overflow-x-hidden bg-inquares-loafer-500 ${openSans.className}`}
+      >
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
