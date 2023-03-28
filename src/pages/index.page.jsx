be6b7ba942effb2589af570e8705625d58445b1e
Ersan Karimi <ersankarimii311@gmail.com>
2023-03-28 21:09:09 +0800

    chore: update tailwindcss classnames

--- src/pages/index.page.jsx
+++ src/pages/index.page.jsx
@@ -9,7 +9,7 @@ import heroImage from "../../public/hero.png";
 
 const Home = () => (
   <>
-    <section className="relative grid grid-cols-1 grid-rows-[max-content_1fr] gap-12 md:grid-rows-none lg:grid-cols-[1fr_1fr] xl:grid-cols-[1.25fr_1fr] 2xl:grid-cols-[1.5fr_1fr]">
+    <section className="grid grid-cols-1 grid-rows-[max-content_1fr] gap-12 md:grid-rows-none lg:grid-cols-[1fr_1fr] xl:grid-cols-[1.25fr_1fr] 2xl:grid-cols-[1.5fr_1fr]">
       <div className="flex h-max flex-col items-center justify-center gap-8 text-center lg:items-start lg:text-left">
         <div className="flex flex-col gap-4 sm:w-5/6 lg:w-full">
           <h3 className="font-semibold uppercase text-inquares-pizazz-600">
@@ -46,7 +46,7 @@ const Home = () => (
       </div>
     </section>
 
-    <section className="relative grid grid-cols-1 grid-rows-[max-content_1fr] items-center gap-12 overflow-hidden rounded-2xl bg-inquares-hippie-green-300 p-4 lg:grid-cols-2 lg:grid-rows-1 lg:py-12">
+    <section className="relative grid grid-cols-1 grid-rows-[max-content_1fr] items-center gap-12 overflow-hidden rounded-2xl bg-inquares-hippie-green-300 p-8 sm:p-8 md:p-12 lg:grid-cols-2 lg:grid-rows-1 lg:py-14">
       <div className="absolute -top-8 left-0 h-96 w-96 rounded-full bg-inquares-hippie-green-400/40 blur-2xl" />
       <div className="absolute -bottom-[20%] right-0 h-96 w-96 rounded-full bg-inquares-hippie-green-400/40 blur-2xl" />
       <picture className="relative flex h-full max-h-[320px] w-full md:max-h-[380px] lg:max-h-[520px]">
@@ -57,11 +57,11 @@ const Home = () => (
           className="h-full w-full object-contain"
         />
       </picture>
-      <div className="relative m-auto flex flex-col gap-8 sm:max-w-[80%]">
-        <div className="flex flex-col gap-1">
-          <h1 className="text-3xl font-bold tracking-wide text-inquares-hippie-green-900 lg:text-4xl">
+      <div className="m-auto flex flex-col gap-8 sm:max-w-[80%]">
+        <div className="flex w-max flex-col gap-1">
+          <h2 className="text-3xl font-bold tracking-wide text-inquares-hippie-green-900 lg:text-4xl 2xl:text-5xl">
             Tentang Kami
-          </h1>
+          </h2>
           <Separator className="h-1 w-1/2 bg-inquares-hippie-green-900" />
         </div>
         <div className="flex flex-col gap-3">
@@ -80,7 +80,7 @@ const Home = () => (
 
         <Link
           href="/"
-          className="group flex w-max items-center gap-2 rounded-md border-2 border-inquares-hippie-green-900/80 px-8 py-2 text-lg font-medium text-inquares-hippie-green-900 outline-none ring-inquares-hippie-green-800/80 transition-all duration-200 hover:bg-inquares-hippie-green-900/90 hover:text-inquares-hippie-green-200 focus:ring-2 active:bg-inquares-hippie-green-900 active:ring-[3px]"
+          className="group flex w-max items-center gap-2 rounded-md border-2 border-inquares-hippie-green-900/80 px-8 py-2 text-base font-medium text-inquares-hippie-green-900 outline-none ring-inquares-hippie-green-800/80 transition-all duration-200 hover:bg-inquares-hippie-green-900/90 hover:text-inquares-hippie-green-200 focus:ring-2 active:bg-inquares-hippie-green-900 active:ring-[3px] lg:text-lg"
         >
           Baca selengkapnya
         </Link>
