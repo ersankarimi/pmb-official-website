6b69db0752f4a520bbe051d4b279d42891037096
Ersan Karimi <ersankarimii311@gmail.com>
2023-04-04 02:46:57 +0800

    chore: update the animation `duration` from 200 to 150

--- src/layouts/root-layout/nav-link.jsx
+++ src/layouts/root-layout/nav-link.jsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
-export const NavLink = ({ children, href, onClick }) => (
+export const NavLink = ({ children, href = "/", onClick = () => {} }) => (
   <Link
     href={href}
     onClick={onClick}
-    className="flex items-center rounded-sm p-1 text-2xl font-semibold text-inquares-blue-whale-500/70 outline-none transition-all duration-200 hover:text-inquares-blue-whale-500 focus:text-inquares-blue-whale-500 focus:ring-2 focus:ring-inquares-hippie-green-400 lg:text-base xl:text-lg"
+    className="flex items-center rounded-sm p-1 text-2xl font-semibold text-inquares-blue-whale-500/70 outline-none transition-all duration-150 hover:text-inquares-blue-whale-500 focus:text-inquares-blue-whale-500 focus:ring-2 focus:ring-inquares-hippie-green-400 lg:text-base xl:text-lg"
   >
     {children}
   </Link>
