1599482e2138a05a09fc68ce601090f52ffae327
Ersan Karimi <ersankarimii311@gmail.com>
2023-04-04 17:55:15 +0800

    chore: refactoring the `metadata` using next `Head`
    Metadata not working with `pages` dir

--- src/layouts/root-layout/root-layout.jsx
+++ src/layouts/root-layout/root-layout.jsx
@@ -1,4 +1,5 @@
 import { Open_Sans as OpenSans } from "next/font/google";
+import Head from "next/head";
 
 import { Footer } from "./footer";
 import { Header } from "./header";
@@ -9,18 +10,18 @@ export const openSans = OpenSans({
   variable: "--font-open-sans",
 });
 
-export const metadata = {
-  title: "PMB HMIF 2023",
-  description: "Website Departemen Pengembangan Minat dan Bakat HMIF 2023",
-  icons: {
-    icon: "/logo-pmb.png",
-    shortcut: "/logo-pmb.png",
-  },
-};
-
 export const RootLayout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>PMB HMIF 2023</title>
+        <meta
+          name="description"
+          content="Website Departemen Pengembangan Minat dan Bakat HMIF 2023"
+        />
+        <link rel="icon" href="/logo-pmb.png" />
+        <link rel="shortcut icon" href="/logo-pmb.png" />
+      </Head>
       <Header />
       <main className="m-auto flex max-w-7xl flex-[2] flex-col gap-40 overflow-x-hidden p-4 lg:container md:p-8 xl:p-12">
         {children}
