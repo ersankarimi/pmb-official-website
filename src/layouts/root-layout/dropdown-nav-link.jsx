dffd8223ae957b2a0fef9ee9fb5ae9dddc0fdc77
Ersan Karimi <ersankarimii311@gmail.com>
2023-04-04 17:35:42 +0800

    chore: add `DropdownMenuPortal` to the contents
    Also renaming the `DropdownMenu` to `DropdownMenuRoot`

--- src/layouts/root-layout/dropdown-nav-link.jsx
+++ src/layouts/root-layout/dropdown-nav-link.jsx
@@ -2,27 +2,29 @@ import Link from "next/link";
 import { MdKeyboardArrowDown } from "react-icons/md";
 
 import {
-  DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuRoot,
   DropdownMenuTrigger,
 } from "@/components";
 
 export const DropdownNavLink = ({
   triggerTextValue = "",
   dropdownItems = [],
-}) => {
-  return (
-    <DropdownMenu modal={false}>
-      <DropdownMenuTrigger asChild>
-        <button
-          type="button"
-          className="group flex items-center rounded-sm p-1 text-2xl font-semibold text-inquares-blue-whale-500/70 outline-none outline-1 transition-all duration-200 hover:text-inquares-blue-whale-500 focus:text-inquares-blue-whale-500 focus:outline-inquares-hippie-green-300 focus:ring-2 focus:ring-inquares-hippie-green-400 lg:text-base xl:text-lg"
-        >
-          {triggerTextValue}
-          <MdKeyboardArrowDown className="text-inquares-blue-whale-500/80 duration-150 group-hover:text-inquares-blue-whale-500 group-data-[state='open']:-rotate-180" />
-        </button>
-      </DropdownMenuTrigger>
+}) => (
+  <DropdownMenuRoot modal={false}>
+    <DropdownMenuTrigger asChild>
+      <button
+        type="button"
+        className="group flex items-center rounded-sm p-1 text-2xl font-semibold text-inquares-blue-whale-500/70 outline-none outline-1 transition-all duration-200 hover:text-inquares-blue-whale-500 focus:text-inquares-blue-whale-500 focus:outline-inquares-hippie-green-300 focus:ring-2 focus:ring-inquares-hippie-green-400 lg:text-base xl:text-lg"
+      >
+        {triggerTextValue}
+        <MdKeyboardArrowDown className="text-inquares-blue-whale-500/80 duration-150 group-hover:text-inquares-blue-whale-500 group-data-[state='open']:-rotate-180" />
+      </button>
+    </DropdownMenuTrigger>
+
+    <DropdownMenuPortal>
       <DropdownMenuContent
         className="z-20 flex flex-col gap-1 border-b-2 border-b-inquares-blue-whale-200 bg-inquares-loafer-500 py-2 sm:w-48 sm:rounded-lg sm:shadow-lg"
         arrowFillClassName="fill-transparent"
@@ -37,6 +39,6 @@ export const DropdownNavLink = ({
           </DropdownMenuItem>
         ))}
       </DropdownMenuContent>
-    </DropdownMenu>
-  );
-};
+    </DropdownMenuPortal>
+  </DropdownMenuRoot>
+);
